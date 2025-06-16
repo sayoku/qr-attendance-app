d8c46b6112b05a59812c7ef0ae07c6c42481813d
Anna Huang <annie.k.huang@icloud.com>
2025-06-16 16:59:45 +0200

    Fixing admin connection to qr code display, tested nrok

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def get_attendance_data():
     return jsonify(attendance_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
