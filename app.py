8ae5a981cf28605ad1abe0c6953feb997c9fbc5e
Anna Huang <annie.k.huang@icloud.com>
2025-08-16 14:29:52 -0400

    add dues tracker to data page

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def get_attendance_data():
     return jsonify(attendance_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001) 
